f30b2268583bd1efe09358ef0d13d6672f730b8a
Anthony Nwokoma <anthonynwokoma1@gmail.com>
2024-06-01 00:14:51 +0100

    New World2

--- resources/views/layouts/index.blade.php
+++ resources/views/layouts/index.blade.php
@@ -15,6 +15,18 @@
             background-size: cover;
             background-repeat: no-repeat;
             background-attachment: fixed;
+
+        }
+
+        body::before {
+            content: '';
+            position: absolute;
+            top: 0;
+            left: 0;
+            right: 0;
+            bottom: 0;
+            background: rgba(255, 255, 255, 0.5); /* White overlay with 50% opacity */
+            z-index: -1;
         }
 
         #pagecontent{
